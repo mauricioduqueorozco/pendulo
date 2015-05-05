3221bc6969c489909a980bc0f30a8292b488e0f3
mauricio duque orozco <mauricio.duque.eje@icloud.com>
2015-05-05 12:10:49 -0500

    pendulo
    Pendulo

---
+++ server.js
@@ -0,0 +1,71 @@
+var PxPorCm = 10,
+		Xo = 0,
+		Yo = 0,
+		alfa = 30 * Math.PI /180,
+		atg = 0,
+		DArc = 0,
+		Dalfa = 0,
+		vtg = 0,
+		x = 200,
+		y = 0,
+		dt = 0.005,
+		t = 0,
+		l = 0.3, // metros longitud de pendulo
+		g = 9.8; // gravity m/s2x
+
+
+
+window.onload = function() {
+	console.log('hi')
+    document.body.style.background = "#f3f3f3";
+
+	var canvas = document.createElement("canvas");
+	canvas.width = 800;
+    canvas.height = 400;
+    document.body.appendChild(canvas);
+	var ctx = canvas.getContext("2d");
+
+	
+	
+
+	var init = function(){
+		ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
+		
+		var puntos_finales = line_Ang({
+										x1 : x,
+										y1 : y,
+										l : l * PxPorCm * 100,
+										ang : alfa,
+										color : 'black'
+									},ctx);
+
+		// punto alto
+		punto({
+				centroX : puntos_finales.desp_X + x,
+				centroY : puntos_finales.desp_Y + y,
+				radio : 10
+		},ctx);
+		linea({x1 : 0,
+					y1 : 0,
+					x2:  200,
+					y2: 0 ,
+					color : 'black'
+		},ctx);
+	}
+	
+	//init(alfa)
+
+	setInterval(function () {
+		t = t + dt;
+		atg = g * Math.sin(alfa); // aceleracion tangengial m / s2
+		vtg = vtg + atg * dt;
+		DArc = vtg * dt;
+		Dalfa = DArc / l; 
+		alfa = alfa - Dalfa;
+		//console.log(alfa * 180 / Math.PI)
+		init(alfa)
+		
+	}, 1);
+
+	
+};
