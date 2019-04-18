35067823889cad736359d796fe29b72cca81d2c5
MauricioDuqueO <mduque@agmatice.com>
2019-04-18 12:42:20 +0300

    Update forces

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var PxPorCm = 10,
+var PxPorCm = 50,
 		Xo = 0,
 		Yo = 0,
-		alfa = 30 * Math.PI /180,
+		alfa = 40 * Math.PI /180,
 		atg = 0,
 		DArc = 0,
 		Dalfa = 0,
@@ -12,31 +12,31 @@ var PxPorCm = 10,
 		c =0,
 		k = -0.1,
 		t = 0,
-		l = 0.3, // metros longitud de pendulo
-		g = 9.8; // gravity m/s2x
+		long_cuerda = 300, // metros longitud de pendulo
+		g = 9.8, // gravity m/s2x
+		mass = 0.1,
+		f1 = 0,
+		f2 = 0;
 
 
 
 window.onload = function() {
-	console.log('hi')
-    document.body.style.background = "#f3f3f3";
+  document.body.style.background = "#f3f3f3";
 
 	var canvas = document.createElement("canvas");
-	canvas.width = 800;
+		canvas.width = 800;
     canvas.height = 400;
     document.body.appendChild(canvas);
 	var ctx = canvas.getContext("2d");
 
-	
-	
 
-	var init = function(){
+	var init = function(alfa){
 		ctx.clearRect ( 0 , 0 , canvas.width, canvas.height );
-		
+
 		var puntos_finales = line_Ang({
 										x1 : x,
 										y1 : y,
-										l : l * PxPorCm * 100,
+										l : long_cuerda,
 										ang : alfa,
 										color : 'black'
 									},ctx);
@@ -56,35 +56,51 @@ window.onload = function() {
 		line_Ang({
 					x1 : puntos_finales.desp_X + x,
 					y1 : puntos_finales.desp_Y + y,
-					l :  0.1 * PxPorCm * 100,
+					l :  mass * g * PxPorCm,
 					ang : 0 * Math.PI / 180,
 					color : 'blue'
-				},ctx);
-		
+		},ctx);
+
 		line_Ang({
 					x1 : puntos_finales.desp_X + x,
 					y1 : puntos_finales.desp_Y + y,
-					l :  0.1 * PxPorCm * 100,
+					// l :  0.1 * PxPorCm,
+					l :  mass * g * Math.cos(alfa) * PxPorCm,
 					ang : alfa,
 					color : 'red'
 		},ctx);
+
+		var state = 0;
+		state = (alfa > 0)?-1:1;
+
+		line_Ang({
+					x1 : puntos_finales.desp_X + x,
+					y1 : puntos_finales.desp_Y + y,
+					l :  mass * g * Math.sin(alfa) * PxPorCm,
+					ang : alfa - (90 * Math.PI / 180),
+					// ang : alfa,
+					color : 'green'
+		},ctx);
 	}
-	
+
 	//init(alfa)
 
 	setInterval(function () {
 		t = t + dt;
-		// reduccion del movimiento por friccion 
-		
+		// reduccion del movimiento por friccion
+
 		atg = g * Math.sin(alfa) + k * vtg; // aceleracion tangengial m / s2
 		vtg = vtg + atg * dt;
 		DArc = vtg * dt;
-		Dalfa = DArc / l; 
+		Dalfa = DArc / (long_cuerda / 1000);
 		alfa = alfa - Dalfa;
-		//console.log(alfa * 180 / Math.PI)
-		init(alfa)
-		
-	}, 1);
 
-	
+		f1 = mass*Math.sin(alfa)/10;
+		console.log("F2 " +mass*Math.cos(alfa));
+		init(alfa)
+		// init()
+
+	}, 100);
+
+
 };
